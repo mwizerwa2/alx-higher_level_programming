c9737fc951f54c6c6cea815662c3868a71b5394e
mwizerwa2 <mfrancoisregis2@gmail.com>
2024-04-15 07:03:43 -0700

    conditionals

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-
 number= int(input("Enter any number "))
 
 if number >0:
     print(f"{number} is postive")
-
 elif number ==0:
     print(f"{number} is zero")
-
 else:
     print(f"{number} is negative")
